2a34cb30a8e16c61d2ef049d92261a42e2304592
Jackie Devin <ofexfoundation@gmail.com>
2024-02-23 21:03:45 +0200

    commit -m "Add TypeScript to the project"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+"use strict";
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const React = __importDefault(require("react"));
+const App = __importDefault(require("./App"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+("use strict");
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = __importDefault(require("react-dom/client"));
+require("./index.css");
+const reportWebVitals_1 = __importDefault(require("./reportWebVitals"));
+const root = client_1.default.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+(0, reportWebVitals_1.default)();
