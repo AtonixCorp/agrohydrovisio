9781fb2ac7fbd1b96be22cf8236fa08f0f5c795b
ofidohub <ofidohub@gmail.com>
2024-08-02 00:25:59 +0200

    commit 1e3f3

--- src/components/Custom/CustomHeader.js
+++ src/components/Custom/CustomHeader.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+
 import './CustomHeader.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
