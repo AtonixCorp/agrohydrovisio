4358499c4a4e84acbe2fbfd5a55b8f86db8db1b0
ofidohub <ofidohub@gmail.com>
2024-08-02 00:18:47 +0200

    commit 1e3f3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import './scss/styles.scss';
-import React, { Suspense, useState, startTransition } from 'react';
+import React, { Suspense } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './analytics';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import HeroSection from './components/HeroSection';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
-import MyComponent from './components/MyComponent';
 import ThemeToggle from './components/ThemeToggle';
 import NavBar from './components/NavBar';
-import { Slider } from 'antd';
 
 import CustomHeader from './components/Custom/CustomHeader';
-
-const SignupLazy = React.lazy(() => import('./account/SignUp'));
-const SignInLazy = React.lazy(() => import('./account/SignIn'));
 
 function App() {
   return (
@@ -32,20 +22,12 @@ function App() {
 }
 
 function AppContent() {
-  const navigate = useNavigate();
-  const [isSignInOpen, setIsSignInOpen] = useState(false);
-  const [isSignUpOpen, setIsSignUpOpen] = useState(false);
-
   return (
     <div className="App">
       <CustomHeader />
 
       <ThemeToggle />
-      <Routes>
-        <Route path="/" element={<MyComponent />} />
-        {isSignUpOpen && <Route path="/signup" element={<SignupLazy />} />}
-        {isSignInOpen && <Route path="/signin" element={<SignInLazy />} />}
-      </Routes>
+
       <div className="container">
         <Suspense fallback={<div>Loading...</div>}>
           <NavBar />
