76441ee38b8980fdbc7e8427faf6a0181bd87070
ofidohub <ofidohub@gmail.com>
2024-08-04 00:45:16 +0200

    commit 1e4b6f1fcc9961a436544d8ccf308e47b710

--- bcend/sendsgrid/messagesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/messages', (req, res) => {
-  res.json({ message: 'This is a message from the backend API' });
-});
-
-module.exports = router;
